51ea4a7919f192d9f0ab380f8d8510d7e0f7fdd8
xuyi <278329118@qq.com>
2026-03-25 19:16:09 +0800

    Add files via upload

---
+++ fasttest/common/__init__.py
@@ -0,0 +1,10 @@
+from xffasttest.common.log import Logger, Fore
+from xffasttest.common.decorator import action
+from xffasttest.common.dict import Dict, DictEncoder
+from xffasttest.common.variables import Variables
+
+gvar = Variables()
+
+logger = Logger()
+
+all = ['gvar', 'logger', 'Fore', 'action', 'Dict']
