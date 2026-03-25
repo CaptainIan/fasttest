877c36a452a3ddbbdaae01cdb537c9a4140b4af4
xuyi <278329118@qq.com>
2026-03-25 19:14:31 +0800

    Add files via upload

---
+++ fasttest/fasttest/driver/__init__.py
@@ -0,0 +1,5 @@
+from xffasttest.driver import driver
+
+driver = driver.Driver()
+
+all = ['driver']
---
+++ fasttest/fasttest/driver/playwright/request.py
@@ -0,0 +1,29 @@
+import json
+
+class Request(object):
+
+    def __init__(self) -> None:
+        self.request_list = []
+
+    def handle_request(self, request):
+        pass
+
+    def handle_response(self, response):
+
+        try:
+            data = {
+                'url': response.url,
+                'request': {
+                    'method': response.request.method,
+                    'headers': response.request.headers,
+                    'data': response.request.post_data_json
+                },
+                'response': {
+                    'ok': response.ok,
+                    'status': response.status,
+                    'data': response.json()
+                }
+            }
+            self.request_list.append(data)
+        except Exception as e:
+            pass
